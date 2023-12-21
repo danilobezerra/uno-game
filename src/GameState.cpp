0a6a85a4adf2bd843be2a79f0f81128c84c35548
Danilo Bezerra <dbezerra@kokku.com.br>
2023-12-21 10:10:48 -0300

    Add scratch methods on `GameState`

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -4,3 +4,10 @@
 
 #include "GameState.h"
 
+std::shared_ptr<Card> GameState::getTopDiscardCard() const {
+    return topDiscardCard;
+}
+
+void GameState::setTopDiscardCard(const std::shared_ptr<Card> &card) {
+    topDiscardCard = card;
+}
